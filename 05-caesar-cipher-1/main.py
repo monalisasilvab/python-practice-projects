94e438ef00c514b0b1bb6017d5fb9c8851bbe588
monalisasilvab <monalisasb91@gmail.com>
2026-01-15 10:53:28 -0300

    feat(caesar-cipher-1): add basic Caesar cipher encoder

---
+++ 05-caesar-cipher-1/main.py
@@ -0,0 +1,15 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+directions = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def encrypt(original_text, shift_amount):
+    cipher_text = ""
+    for letter in original_text:
+        shifted_position = alphabet.index(letter) + shift_amount
+        shifted_position %= len(alphabet)
+        cipher_text += alphabet[shifted_position]
+    print(f"Here is the encode result: {cipher_text}")
+
+encrypt(original_text=text,shift_amount=shift)
